235764f724898b72c4ff4a740f5b9bf73830c183
themishkaCoder <45307191+themishkaCoder@users.noreply.github.com>
2019-03-03 12:08:49 +0200

    homework

---
+++ portfolio/jsEvents/color/main.js
@@ -0,0 +1,5 @@
+var box = document.getElementById('box');
+
+box.addEventListener("click", function (e) {
+  e.target.style.background = "green";
+}, false)
---
+++ portfolio/jsEvents/forms/main.js
@@ -0,0 +1,5 @@
+var a = document.forms[0];
+a.in1.value = "ddd";
+
+var b = document.form2;
+b.in2.value = "ff45";
