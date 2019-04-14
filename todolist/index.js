73e5fbfc9bba35d92b2e7e27dc94591246ca0f99
themishkaCoder <45307191+themishkaCoder@users.noreply.github.com>
2019-04-14 14:25:25 +0300

    for addTask added regularka

--- todolist/index.js
+++ todolist/index.js
@@ -6,12 +6,28 @@
     var inputTask = document.getElementById("inputTask")
     var completeBtns = document.getElementsByClassName("completeBtns")
 
+    addTask.style.backgroundColor = "gray"
+    addTask.innerText = "Выключено"
+
+    inputTask.addEventListener("input", function(){
+        addTask.style.backgroundColor = "green"
+        addTask.innerText = "Добавить задачу"
+        
+    }, false)
+
     addTask.addEventListener("click", function(){
         var listItem = document.createElement("div")
-        listItem.className = "listItem"
-        listItem.innerHTML = `<p>${inputTask.value}</p> <button class="completeBtns" title="Выполнено">✓</button> <button class="delButns" title="Удалить">✗</button>`
-        list.appendChild(listItem)
-        inputTask.value = ""
+        if(inputTask.value == ""){
+            console.log("error")
+            addTask.style.backgroundColor = "gray"
+            addTask.innerText = "Выключено"
+        }else{
+            // addTask.style.backgroundColor = "green"
+            listItem.className = "listItem"
+            listItem.innerHTML = `<p>${inputTask.value}</p> <button class="completeBtns" title="Выполнено">✓</button> <button class="delButns" title="Удалить">✗</button>`
+            list.appendChild(listItem)
+            inputTask.value = ""
+        }
         for (let delBut of delButns){
             delBut.addEventListener("click", function(e){
 
