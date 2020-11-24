a23b0a34eb29ff9d4389726ffe2f9a305ff66883
BearVloger <41472327+BearVloger@users.noreply.github.com>
2020-11-24 12:58:39 +0200

    full main page

--- prosecurity/js/app.js
+++ prosecurity/js/app.js
@@ -1,6 +1,33 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    adaptiveMenu: "adaptive-menu",
+    activeMenu: false
+  },
+  methods: {
+    moveDown: function () {
+      window.scrollTo({
+        top: 5000,
+        behavior: "smooth"
+      });
+      console.log("Test")
+    },
+
+    showHideMenu: function () {
+      this.activeMenu == false ? (this.adaptiveMenu = "adaptive-menu adaptive-menu-show", this.activeMenu = true) : (this.adaptiveMenu = "adaptive-menu adaptive-menu-hide", this.activeMenu = false)
     }
-  })
+  }
+})
+
+
+  // const scrollToTop = () => {
+  //   const c = document.documentElement.scrollTop || document.body.scrollTop;
+  //   if (c > 0) {
+  //     window.requestAnimationFrame(scrollToTop);
+  //     window.scrollTo(0, c - c / 8);
+  //   }
+  // };
+
+  // // Example
+  // scrollToTop();
